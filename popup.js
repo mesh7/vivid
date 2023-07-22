05292dac7021bcb60bf026a69b27fe48b2edb379
Shubham Meshram <meshram.shubham27@gmail.com>
2023-07-23 01:36:11 +0530

    added function to fetch the color from eye-dropper

--- popup.js
+++ popup.js
@@ -1,15 +1,35 @@
-const button = document.querySelector('.colorPickerButton');
+// Scope of the variables are not available in the script which gets executed
+// in [chrome.scripting.executeScript] until passed in the arguments as params
+const colorPickerButton = document.querySelector(".colorPickerButton");
+const colorGrid = document.querySelector(".colorGrid");
+const colorValue = document.querySelector(".colorValue");
 
-button.addEventListener('click', async() => {
-    console.log('Listening')
-    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+colorPickerButton.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: pickColor,
-    });
-})
+  // Executing the script in the active tab that we fetched
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: pickColor,
+    },
+    async (result) => {
+      const [data] = result;
+      if (data?.result) {
+        const color = data.result.sRGBHex;
+        colorGrid.style.backgroundColor = color;
+        colorValue.innerText = color;
+      }
+    }
+  );
+});
 
-function pickColor() {
-    console.log('Its working')
+// Above this point all the things get executed in the chrome extension
+
+// Function gets executed in the active tab window
+async function pickColor() {
+  try {
+    const eyeDropper = new EyeDropper();
+    return await eyeDropper.open();
+  } catch (error) {}
 }
