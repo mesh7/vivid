461103003078ce9d9406a43942dbc98c66c2a394
Shubham Meshram <meshram.shubham27@gmail.com>
2023-07-22 23:56:30 +0530

    added chrome tabs API to fetch tab information

--- popup.js
+++ popup.js
@@ -1,1 +1,15 @@
-console.log('This is Vivid');
+const button = document.querySelector('.colorPickerButton');
+
+button.addEventListener('click', async() => {
+    console.log('Listening')
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: pickColor,
+    });
+})
+
+function pickColor() {
+    console.log('Its working')
+}
